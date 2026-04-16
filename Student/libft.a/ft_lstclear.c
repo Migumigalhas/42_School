9e685ecf4d203860f9067e17cbb88e97737b3046
Miguel Luiz Gomes Da Silva Sobral <miggomes@c1r4s1.42lisboa.com>
2026-04-16 19:00:55 +0100

    some more

--- Student/libft.a/ft_lstclear.c
+++ Student/libft.a/ft_lstclear.c
@@ -6,10 +6,11 @@
 /*   By: miggomes <miggomes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 12:47:28 by miggomes          #+#    #+#             */
-/*   Updated: 2026/04/16 16:36:03 by miggomes         ###   ########.fr       */
+/*   Updated: 2026/04/16 16:47:21 by miggomes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
+
