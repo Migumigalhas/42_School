bbda259146e5cfd6e5ea1eb08ee354ceabadfd48
Miguel Luiz Gomes Da Silva Sobral <miggomes@c1r2s10.42lisboa.com>
2026-04-17 15:01:08 +0100

    some corrections 2

--- Student/libft.a/ft_memchr.c
+++ Student/libft.a/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: miggomes <miggomes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 12:50:31 by miggomes          #+#    #+#             */
-/*   Updated: 2026/04/17 14:45:58 by miggomes         ###   ########.fr       */
+/*   Updated: 2026/04/17 15:01:02 by miggomes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	int i;
-	char *str;
+	size_t i;
+	const char *str;
 
+	i = 0;
 	str = (const char *)s;
-	
+	while (i < n)
+	{
+		if (str[i] == (const char)c)
+			return ((void *)&str[i]);
+		i++;
+	}
+	return (NULL);
 }
